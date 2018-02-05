d95a61d6a574ab4afe8f4750140fc1e4212c1320
Leghuir <qwerty1214@gmail.com>
2018-02-05 22:04:34 +0100

    Factory Method structure created

---
+++ EmailSender/Models/Products/IEmail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailSender.Models
+{
+    public interface IEmail
+    {
+        String GetSender();
+        String GetRecipient();
+        String GetMessage();
+        String GetSubject();
+        List<String> GetAttachments();
+    }
+}
---
+++ EmailSender/Models/Sender.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+namespace EmailSender.Models
+{
+    public class Sender
+    {
+        public bool Send(IEmail email)
+        {
+            MailMessage mail = new MailMessage(email.GetSender(), email.GetRecipient());
+            SmtpClient client = new SmtpClient();
+            client.Port = 587;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Host = "smtp.gmail.com";
+            mail.Subject = email.GetSubject();
+            mail.Body = email.GetMessage();
+            for(int i=0;i<email.GetAttachments().Count;i++)
+            {
+                mail.Attachments.Add(new Attachment(email.GetAttachments().ElementAt(i)));
+            }
+            client.Send(mail);
+            return false;
+        }
+    }
+}
